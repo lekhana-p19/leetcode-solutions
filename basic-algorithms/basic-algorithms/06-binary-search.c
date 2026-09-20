1acc754a551116363a018b15f15e959c6d044a36
lekhana <lekhanap2006@gmail.com>
2026-09-20 18:24:34 +0530

    Add second test case for Binary Search

--- basic-algorithms/basic-algorithms/06-binary-search.c
+++ basic-algorithms/basic-algorithms/06-binary-search.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int search(int *nums, int numsSize, int target)
+int search(int* nums, int numsSize, int target)
 {
     int left = 0;
     int right = numsSize - 1;
@@ -10,18 +10,12 @@ int search(int *nums, int numsSize, int target)
         int mid = (left + right) / 2;
 
         if (nums[mid] == target)
-        {
             return mid;
-        }
 
         if (nums[mid] < target)
-        {
             left = mid + 1;
-        }
         else
-        {
             right = mid - 1;
-        }
     }
 
     return -1;
@@ -30,11 +24,12 @@ int search(int *nums, int numsSize, int target)
 int main()
 {
     int nums[] = {-1, 0, 3, 5, 9, 12};
-    int target = 9;
 
-    int result = search(nums, 6, target);
+    int result1 = search(nums, 6, 9);
+    printf("Test 1: Index = %d (Expected: 4)\n", result1);
 
-    printf("Index = %d\n", result);
+    int result2 = search(nums, 6, 2);
+    printf("Test 2: Index = %d (Expected: -1)\n", result2);
 
     return 0;
 }
